af067ce7aeab3348480f4e4db77859ea5f2e5b75
VLAD KING (КОРОЛЬ) <nervcop@gmail.com>
2026-03-11 15:38:01 +0300

    fix(flake): rename config name

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.vps-hostname = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         "/etc/nixos/configuration.nix"
